712fc84be897435814387a715564fa4a6266c749
kiethohoai <kietho.060193@gmail.com>
2024-07-28 16:09:55 +0700

    Bai-4-Router-get-list-student

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5173;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// config routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/students', (req, res) => {
+  res.send('Student List');
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
