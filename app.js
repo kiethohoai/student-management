72f7bd5fc34f2a64123118703b1ceade23d18e89
kiethohoai <kietho.060193@gmail.com>
2024-07-28 16:29:36 +0700

    Bai-5-Router-get-detail-student

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const port = 5173;
 
 // config routes
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Home Page');
 });
 
 app.get('/students', (req, res) => {
   res.send('Student List');
+});
+
+app.get('/students/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Student Details ID: ${id}`);
 });
 
 app.listen(port, () => {
